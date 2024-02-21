641714574aa4dff2b70ae772629a7e64a3367f6e
rutukavathiya <155123711+rutukavathiya@users.noreply.github.com>
2024-02-21 11:17:37 +0530

    Add files via upload

---
+++ dash_board/deshboard_input.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<form  action="deshboard.php" method="post">
+<input type="text" name="aboutme"/><br>
+<input type="text" name="myskill"/><br>
+<input type="submit" name="submit"/><br>
+</form>
+</body>
+</html>
+
+
+
+
